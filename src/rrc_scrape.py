1df25cfe0eb87f090e51e80d551163e896fcb3d9
Mark Young <mark.francis.young@gmail.com>
2024-05-26 12:34:23 -0500

    Dockerfile added for app inclusion on GCP. Removal of Locator.py and test Symbol table redefine and occurs handling. Add selenium code w/headless Chrome browser to get RRC dataset files Add symbol/symbol table unit tests.

--- src/rrc_scrape.py
+++ src/rrc_scrape.py
@@ -3,12 +3,19 @@ import time
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")
+
 
 # this program gets the URLs for the dataset section
 # next it will make a list of URLs for each of the files section by section
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.rrc.texas.gov/resource-center/research/data-sets-available-for-download/")
+settings = {'behavior': 'allow', 'downloadPath': '/Users/markyoung/data'}
+driver.execute_cdp_cmd('Browser.setDownloadBehavior', settings)
 
 # Find the Data Sets H2 element.
 h2 = driver.find_element(By.XPATH, "//article/h2[text()='Data Sets']")
@@ -35,8 +42,19 @@ df = pd.DataFrame(zip(download_types, href_list, data_set_names),
                   columns=['filetype', 'uri', 'dataset_description'])
 
 time_start = time.time()
-for uri in list(df['uri']):
+oil_df = df[df['dataset_description'].str.contains('Oil')]
+# for testing. remove next line
+oil_df = oil_df.head(1)
+for uri in list(oil_df['uri']):
     driver.get(uri)
+    anchors = driver.find_elements(By.XPATH, "//a")[1:]
+    for anchor in anchors:
+        ac = (ActionChains(driver)
+              .move_to_element(anchor)
+              .click()).perform()
+        time.sleep(2)
+
+
 time_end = time.time()
-elapsed_sec = str((time_end - time_start) / 60.0)
-print(f'time for all pages: in minutes {elapsed_sec}')
+elapsed_sec = (time_end - time_start) / 60.0
+print(f'time for all pages: in minutes {elapsed_sec:.2f}')
